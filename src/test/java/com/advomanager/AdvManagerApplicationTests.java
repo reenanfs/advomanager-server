d5798217c723d63fbfd40093c02172566edda3a7
reenanfs <renan.frsantana@gmail.com>
2025-07-07 20:22:59 -0300

    chore: renamed app

--- src/test/java/com/advomanager/AdvManagerApplicationTests.java
+++ src/test/java/com/advomanager/AdvManagerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.advmanager;
+package com.advomanager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AdvManagerApplicationTests {
+class advomanagerApplicationTests {
 
 	@Test
 	void contextLoads() {
