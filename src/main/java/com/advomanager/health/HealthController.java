825f0d9f2d4fe91aaa62e3a3f57ea78868fe31c9
reenanfs <renan.frsantana@gmail.com>
2025-08-04 23:48:57 -0300

    refactor: nullable false

--- src/main/java/com/advomanager/health/HealthController.java
+++ src/main/java/com/advomanager/health/HealthController.java
@@ -1,5 +1,7 @@
 package com.advomanager.health;
 
+import java.time.Instant;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthController {
 	@GetMapping
 	public String healthCheck() {
+		System.out.println(Instant.now() + "The server is up and running");
 		return "The server is up and running";
 	}
 }
--- src/main/java/com/advomanager/user/User.java
+++ src/main/java/com/advomanager/user/User.java
@@ -15,7 +15,7 @@ public class User {
     @Column(name = "email", nullable = false, unique = true)
     private String email;
     
-    @Column(name = "name",  nullable = true)
+    @Column(name = "name",  nullable = false)
     private String name;
 
     @Column(name = "is_active", nullable = false)
